5bb79508cf54267100ea5e3dbfdc34cb223020b8
방준호 <yureka0920@naver.com>
2023-12-21 14:36:31 +0900

    Initial commit

---
+++ paymentDomainProject/src/main/java/com/payment/paymentDomainProject/PaymentDomainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.payment.paymentDomainProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentDomainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentDomainProjectApplication.class, args);
+	}
+
+}
---
+++ paymentDomainProject/src/test/java/com/payment/paymentDomainProject/PaymentDomainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.paymentDomainProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentDomainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
